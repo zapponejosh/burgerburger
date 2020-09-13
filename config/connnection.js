063fd7a13540ee4dc91dffda7993f8b6d76b1dea
Josh Zappone <zappone.josh@gmail.com>
2020-09-12 17:26:29 -0700

    adds JAWSDB connection

--- config/connnection.js
+++ config/connnection.js
@@ -1,12 +1,17 @@
 const util = require('util');
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'burgers_db',
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'burgers_db',
+  });
+}
 
 connection.connect((err) => {
   if (err) {
